52ea6a06cd83a3ac20e22095b4882949605e4af8
marcosbottino <mbottino@hms-tech.com>
2024-01-20 12:13:56 -0300

    First Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from 'sonner'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute='class' defaultTheme='system'>
+        {children}
+        <Toaster/>
+        </ThemeProvider>
+        </body>
     </html>
   )
 }
